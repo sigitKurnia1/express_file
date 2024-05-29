5e2ba43f4c9c2aa4f647cb67e799cb65c057b845
Sigit Kurniawan <93625275+sigitKurnia1@users.noreply.github.com>
2024-05-29 11:05:28 +0700

    First Commit

---
+++ backend/controllers/ProductController.js
@@ -0,0 +1,122 @@
+import Product from "../models/ProductModels.js"
+import path from "path"
+import fs from "fs"
+
+export const getProducts = async (req, res) => {
+    try {
+        const response = await Product.findAll()
+        res.json(response)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const getProductById = async (req, res) => {
+    try {
+        const response = await Product.findOne({
+            where: {
+                id : req.params.id
+            }
+        })
+        res.json(response)
+    } catch (error) {
+        console.log(error.message)   
+    }
+}
+
+export const saveProduct = (req, res) => {
+    if (req.files === null) {
+        return res.status(400).json({msg: "No file uploaded"})
+    } else {
+        const name = req.body.title
+        const file = req.files.file
+        const fileSize = file.data.length
+        const ext = path.extname(file.name)
+        const fileName = file.md5 + ext
+        const url = `${req.protocol}://${req.get("host")}//images/${fileName}`
+        const allowedType = ['.png', '.jpg', '.jpeg']
+
+        if (!allowedType.includes(ext.toLocaleLowerCase())) return res.status(422).json({msg: "Invalid images"})
+        
+        if (fileSize > 5000000) return res.status(422).json({msg: "Images must be less than 5MB"})
+
+        file.mv(`./public/images/${fileName}`, async(err) => {
+            if (err) return res.status(500).json({msg: err.message})
+
+            try {
+                await Product.create({name: name, image: fileName, url: url})
+                res.status(201).json({msg: "Product Created Successfully!"})
+            } catch (error) {
+                console.log(error.message)
+            }
+        })
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    const product = await Product.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    if (!product) return res.status(404).json({msg: "No Data Found"})
+
+    let fileName = product.image
+
+    if (req.files && req.files.file) {
+        const file = req.files.file
+        const fileSize = file.data.length
+        const ext = path.extname(file.name)
+        fileName = file.md5 + ext
+        const allowedType = ['.png', '.jpg', '.jpeg']
+
+        if (!allowedType.includes(ext.toLocaleLowerCase())) return res.status(422).json({msg: "Invalid images"})
+
+        if (fileSize > 5000000) return res.status(422).json({msg: "Images must be less than 5MB"})
+
+        const filepath = `./public/images/${product.image}`
+        fs.unlinkSync(filepath)
+
+        file.mv(`./public/images/${fileName}`, (err) => {
+            if (err) return res.status(500).json({msg: err.message})
+        })
+    }
+
+    const name = req.body.title
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`
+
+    try {
+        await Product.update({ name: name, image: fileName, url: url }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json({msg: "Product Updated Successfully"})
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    const product = await Product.findOne({
+        where: {
+            id : req.params.id
+        }
+    })
+
+    if (!product) return res.status(404).json({msg: "No Data Found"})
+
+    try {
+        const filepath = `./public/images/${product.image}`
+        fs.unlinkSync(filepath)
+        await Product.destroy({
+            where: {
+                id : req.params.id
+            }
+        })
+        res.status(200).json({msg: "Product Deleted Successfully!"})
+    } catch (error) {
+        console.log(error.message)
+    }
+}
---
+++ frontend/src/components/ProductList.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
+const ProductList = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    const getProducts = async () => {
+        const response = await axios.get("http://localhost:5000/products")
+        setProducts(response.data)
+    }
+
+    const deleteProduct = async (productId) => {
+        try {
+            await axios.delete(`http://localhost:5000/products/${productId}`)
+            getProducts()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div className="container mt-5">
+        <Link className="button is-success mb-5" to="/add">Add New</Link>
+            <div className="columns is-multiline">
+            {
+                products.map((product) => (
+                    <div className="column is-one-quarter" key={product.id}>
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src={product.url} alt="Image"/>
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-content">
+                                        <p className="title is-4">{product.name}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <footer className="card-footer">
+                                <Link className="card-footer-item" to={`edit/${product.id}`}>Edit</Link>
+                                <a className="card-footer-item" onClick={() => deleteProduct(product.id)}>Delete</a>
+                            </footer>
+                        </div>
+                    </div>
+                ))
+            }
+            </div>
+        </div>
+    )
+}
+
+export default ProductList
